72d0d382741c02bc5afdfdab84a53dad7c676dbf
Lucas Massoni <lucas.masson0307@gmail.com>
2026-05-04 17:25:29 -0300

    .

---
+++ Aula-14-java/src/br/com/fiap/bean/Funcionario.java
@@ -0,0 +1,35 @@
+package br.com.fiap.bean;
+
+import java.time.LocalDate;
+
+public class Funcionario {
+    private String Funcionario;
+    private float valorHoradeTrabalho;
+    private LocalDate dataNascimento;
+
+    public Funcionario(String funcionario, float valorHoradeTrabalho, LocalDate dataNascimento) {
+        Funcionario = funcionario;
+        this.valorHoradeTrabalho = valorHoradeTrabalho;
+        this.dataNascimento = dataNascimento;
+    }
+
+    public float getValorHoradeTrabalho() {
+        return valorHoradeTrabalho;
+    }
+
+    public void setValorHoradeTrabalho(float valorHoradeTrabalho) {
+        this.valorHoradeTrabalho = valorHoradeTrabalho;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public float calcularSalario(){
+        (valorHoradeTrabalho
+    };
+}
