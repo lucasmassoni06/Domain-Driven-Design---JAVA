d8cc98a5a45e9185b2bc6f752e599f0d118f301d
Lucas Massoni <lucas.masson0307@gmail.com>
2026-08-12 15:03:01 -0300

    HashMap com Stream

--- Aula-17-java/src/br/com/fiap/main/PokemonComStream.java
+++ Aula-17-java/src/br/com/fiap/main/PokemonComStream.java
@@ -2,34 +2,40 @@ package br.com.fiap.main;
 
 import javax.swing.*;
 import java.util.HashMap;
+import java.util.List;
+
+import static java.lang.Integer.parseInt;
+import static javax.swing.JOptionPane.*;
 
 public class PokemonComStream {
-    static void main(){
-        HashMap<String, String> mapa = new HashMap<>();
+    static void main() {
+        //Cria lista pokemon
+        HashMap<String, String> listaPokemon = new HashMap<>();
+
+        //Cria o tipo e o nome do pokemon
+        String tipo, pokemon;
+
+        //Menu QUER CRIAR MAIS ALGUM POKEMON
         do {
-            try{
-                String pokemon, tipo;
-                do {
-                    tipo = JOptionPane.showInputDialog(null, "Digite o tipo do seu Pokemon ou digite \"fim\" para finalizar").toUpperCase();
-                    if (!tipo.equals("FIM")){
-                        pokemon = JOptionPane.showInputDialog(null, "Digite o nome do seu pokemon");
-                        if (mapa.containsKey(tipo)){
-                            if (mapa.get(tipo).contains(pokemon)){
-                                JOptionPane.showConfirmDialog(null, "Esse pokemon ja foi registrado!");
-                            }
-                        } else {
-                            mapa.put(tipo, pokemon);
-                        }
-                    }
-
-                } while(!tipo.equals("FIM"));
-
-                String
-
-            } catch (Exception e) {
-                JOptionPane.showMessageDialog(null, e.getMessage(), "Erro", JOptionPane.WARNING_MESSAGE);
+            tipo = JOptionPane.showInputDialog("Qual o tipo do seu pokemon?");
+            pokemon = JOptionPane.showInputDialog("Qual o nome do seu pokemon?");
+            if (listaPokemon.containsKey(pokemon)){
+                JOptionPane.showMessageDialog(null, "Esse pokemon ja foi adicionado!");
+            } else {
+                listaPokemon.put(tipo, pokemon);
             }
-        } while (JOptionPane.showConfirmDialog(null, "Deseja Continuar?", "Atenção", JOptionPane.YES_NO_OPTION, JOptionPane.WARNING_MESSAGE) == 0);
 
+        }while (JOptionPane.showConfirmDialog(null, "Deseja Continuar? ", "Atenção", JOptionPane.YES_NO_OPTION, JOptionPane.WARNING_MESSAGE) == 0);
+        String escolha;
+        do{
+            escolha = JOptionPane.showInputDialog("Qual tipo deseja vizualizar?");
+            if (listaPokemon.containsKey(escolha)){
+                JOptionPane.showMessageDialog(null, "O tipo escolhido foi " + escolha + " E tem os seguintes pokemons" + listaPokemon.c;
+            }else{
+                JOptionPane.showMessageDialog(null, "Tipo de pokemon não existente!");
+            }
+        }while (listaPokemon.containsKey(escolha));
+        JOptionPane.showMessageDialog(null, "FIM");
     }
 }
+
