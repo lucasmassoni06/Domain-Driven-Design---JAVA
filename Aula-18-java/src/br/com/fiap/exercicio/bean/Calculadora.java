7bde601b41a1088951ade2d91aed48ada261c214
Lucas Massoni <lucas.masson0307@gmail.com>
2026-08-19 13:03:41 -0300

    Update Calculadora.java

--- Aula-18-java/src/br/com/fiap/exercicio/bean/Calculadora.java
+++ Aula-18-java/src/br/com/fiap/exercicio/bean/Calculadora.java
@@ -47,7 +47,7 @@ public class Calculadora {
 
     public float divisao(float numero1, float numero2){
         if (numero1 == 0 || numero1 == 0) {
-            
+
         } else {
             return numero1 / numero2;
         }
